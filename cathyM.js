12ad5d0f3fd1769d84fa625d98d0c26008e64cbb
catmiller07 <catmiller07@comcast.net>
2015-10-13 19:19:16 -0400

    Correct Test Questions
    Discussed fixes to problems with instructor.

--- cathyM.js
+++ cathyM.js
@@ -1,9 +1,9 @@
 //Question 1
- $('#contact-div').find('a').html();
+ $('#contact-div').text();
 
 //Question 2
 
-$("#card-text);  
+$("#card-text").find("p";  
   
 //Question3
 $("#card-text").find("p").html("");
@@ -11,16 +11,16 @@ $("#card-text").find("p").html("");
 $(document).ready(function(){
  
   $('#contact-div').on('click',function(){
-      $("#card-text").find("p").html("");
+      $("#card-text").html("");
        });
-)};
+});
 
 //Question 5
- var newText1= $('<form method=\”post" action="submit.php>"'); 
+ var newText1= $('<form method=\”post" action="submit.php">'); 
  var newText2=$('<span>Name:</span><input type=”text" name=”name” id="form-nm">');
-  var newText3=$('<span>Phone number</span><input type=”text”    name=” phone" id="form-ph>');
-  var newText4= $(''<span>Email:</span><input type=”text” name="email" id="form-e">');
-   var newText5=$('<button type=”submit”>Submit</button></form>');   
+ var newText3=$('<span>Phone number</span><input type=”text”    name=”phone" id="form-ph">');
+ var newText4= $('<span>Email:</span><input type=”text” name="email" id="form-e">');
+ var newText5=$('<button type=”submit”>Submit</button></form>');   
 
    $('#contact-div').on('click',function(){
 		  $("#card-text").find("p").html("");
@@ -29,30 +29,29 @@ $(document).ready(function(){
    });
 
 //Question 6 
-var newText1= $('<form method=\”post" action="submit.php>"'); 
+var newText1= $('<form method=\”post" action="submit.php">'); 
  var newText2=$('<span>Name:</span><input type=”text" name=”name” id="form-nm">');
-  var newText3=$('<span>Phone number</span><input type=”text”    name=” phone" id="form-ph>');
-  var newText4= $(''<span>Email:</span><input type=”text” name="email" id="form-e">');
-   var newText5=$('<button type=”submit”>Submit</button></form>');   
-
+ var newText3=$('<span>Phone number</span><input type=”text”    name=”phone" id="form-ph">');
+ var newText4= $('<span>Email:</span><input type=”text” name="email" id="form-e">');
+ var newText5=$('<button type=”submit”>Submit</button></form>');   
    $('#contact-div').on('click',function(){
 		  $("#card-text").find("p").html("");
 	                       $('.card-content')
 	                          .append(newText1,newText2,newText3,newText4,newText5);
-           $('.card-action').find('a').html('');
+           $('.card-action').find('a').css({'display':'none'});
    })
    //Question 7
    $(document).ready(function){
-     var newText1= $('<form method=\”post" action="submit.php>"'); 
-     var newText2=$('<span>Name:</span><input type=”text" name=”name” id="form-nm">');
-     var newText3=$('<span>Phone number</span><input type=”text”    name=” phone" id="form-ph>');
-     var newText4= $('<span>Email:</span><input type=”text” name="email" id="form-e">');
-     var newText5=$('<button type=”submit”>Submit</button></form>');   
+    var newText1= $('<form method=\”post" action="submit.php">'); 
+    var newText2=$('<span>Name:</span><input type=”text" name=”name” id="form-nm">');
+    var newText3=$('<span>Phone number</span><input type=”text”    name=”phone" id="form-ph">');
+    var newText4= $('<span>Email:</span><input type=”text” name="email" id="form-e">');
+    var newText5=$('<button type=”submit”>Submit</button></form>');   
 
        $('#contact-div').on('click',function(){
 		  $("#card-text").find("p").html("");
 	                       $('.card-content')
 	                          .append(newText1,newText2,newText3,newText4,newText5);
-           $('.card-action').find('a').html('');
+        $('.card-action').find('a').css({'display':'none'});
         })  
    });
